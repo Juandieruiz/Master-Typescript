4fae07dd7310ce33518683dd084e64f37f084975
Juan Diego Gómez Ruiz <juandieruiz6@gmail.com>
2022-03-11 17:59:54 +0100

    js learning

--- 00-Repaso/06-aprende-js/main.js
+++ 00-Repaso/06-aprende-js/main.js
@@ -1,6 +1,6 @@
 var nombre = "Juan Diego Gómez";
 var altura = 185;
-
+/*
 var concatenando = "Soy" + nombre + " " + altura;
 
 var datos = document.getElementById("datos");
@@ -17,4 +17,29 @@ if(altura >= 185){
 
 for(var i = 2000; i <= 2020; i++){
     datos.innerHTML += "<h2>Estamos en el año: "+ i;
+}
+*/
+
+function muestraMiNombre(nombre, altura){
+        var misDatos = `
+            <h1> Soy la caja de datos </h1>
+            <h2>Mi nombre es: ${nombre} y mido ${altura}
+        `;
+
+        return misDatos;
+}
+
+function imprimir(){
+    var datos = document.getElementById("datos");
+    datos.innerHTML = muestraMiNombre("pepe", 180);
+}
+
+imprimir();
+
+var nombres = ['Juan','Diego', 'Luis'];
+alert(nombres[1]);
+
+document.write('<h1>Listado de Nombres</h1>')
+for( i = 0; i < nombres.length; i++){
+    document.write(nombres[i] + '<br/>');
 }
